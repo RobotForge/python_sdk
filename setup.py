cd0d86e87b1cff798e124ea9f5fc332c5ecc004a
kegbo <ibokkegbo@gmail.com>
2025-10-29 00:57:56 +0100

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ Setup configuration for the Telemetry SDK package
 from setuptools import setup, find_packages
 from pathlib import Path
 
-VERSION = "0.1.6"
+VERSION = "0.1.7"
 
 # Read long description from README
 def get_long_description():
